83f03305d8613395737e83c72aaf91c4f38e3850
Miss-nonso <chinonsodaniels@yahoo.com>
2023-02-23 10:10:37 -0800

    Edited file 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,20 +9,21 @@
  */
 
 int largest_number(int a, int b, int c)
-{int largest;
+{
+    int largest;
 
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+    if ((a > b && b > c) || (a > c && c > b))
+    {
+        largest = a;
+    }
+    else if ((b > a && a > c) || (b > c && c > a))
+    {
+        largest = b;
+    }
+    else if (( c > a && a > b) || (c > b && b > a))
+    {
+        largest = c;
+    }
 
-return (largest);
+    return (largest);
 }
